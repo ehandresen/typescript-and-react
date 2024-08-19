f3dd78542b4f5d7b65bebaf3b88bc368eaab668a
ehandresen <eirik.horgen.andresen@gmail.com>
2024-08-19 13:17:01 +0200

    props. children

--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,13 +1,24 @@
-interface ComponentProps {
+import React from 'react';
+import { type PropsWithChildren } from 'react';
+
+// type ComponentProps = {
+//   name: string;
+//   id: number;
+//   children?: React.ReactNode;
+// };
+
+type ComponentProps = PropsWithChildren<{
   name: string;
   id: number;
-}
+  children: React.ReactNode;
+}>;
 
-function Component(props: ComponentProps) {
+function Component({ name, id, children }: ComponentProps) {
   return (
     <div>
-      <h2>name: {props.name}</h2>
-      <h2>id: {props.id}</h2>
+      <h2>name: {name}</h2>
+      <h2>id: {id}</h2>
+      {children}
     </div>
   );
 }
