8afafc72bd990c1a6cd16c93a5ef20dc505add93
ehandresen <eirik.horgen.andresen@gmail.com>
2024-08-20 14:57:31 +0200

    ChangeEvent

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,8 +1,35 @@
+import { ChangeEvent, useState } from 'react';
+
 function Component() {
+  const [text, setText] = useState('');
+  const [email, setEmail] = useState('');
+
+  // here we need to set the type of the event ourself's, or else it will be type: any
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
+      <h2>events example</h2>
+      <form className="form">
+        <input
+          type="text"
+          className="form-input mb-1"
+          value={text}
+          onChange={(e) => setText(e.target.value)} // for inline typescript infers the type of the event for us
+        />
+        <input
+          type="email"
+          className="form-input mb-1"
+          value={email}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
     </div>
   );
 }
