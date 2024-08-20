f28b8fd49237a58d0d1ec17faa0453f7e8df32a7
ehandresen <eirik.horgen.andresen@gmail.com>
2024-08-20 15:19:08 +0200

    form event

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,28 +1,49 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
+
+type Person = {
+  name: string;
+};
 
 function Component() {
   const [text, setText] = useState('');
   const [email, setEmail] = useState('');
 
   // here we need to set the type of the event ourself's, or else it will be type: any
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    // const formData = new FormData(e.target as HTMLFormElement);
+
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const text = formData.get('text') as string;
+
+    const person: Person = {
+      name: data.text as string,
+    };
   };
 
   return (
     <div>
       <h2>events example</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
           className="form-input mb-1"
+          name="text"
           value={text}
-          onChange={(e) => setText(e.target.value)} // for inline typescript infers the type of the event for us
+          onChange={(e) => setText(e.target.value)} // for inline function typescript will infers the type of the event for us
         />
         <input
           type="email"
           className="form-input mb-1"
+          name="email"
           value={email}
           onChange={handleChange}
         />
